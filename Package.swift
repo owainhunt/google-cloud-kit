9e7e2f0a178fb2a2d293bc3cae00618529ee95c7
Andrew Edwards <andrew.edwards@saks.com>
2022-06-24 23:29:58 -0400

    added storage to GCK

--- Package.swift
+++ Package.swift
@@ -13,7 +13,8 @@ let package = Package(
             name: "GoogleCloudKit",
             targets: ["Core",
                       "SecretManager",
-                      "Translation"] //"Storage", "Datastore", "PubSub"
+                      "Translation",
+                      "Storage"] //, "Datastore", "PubSub"
         ),
         .library(
             name: "GoogleCloudCore",
