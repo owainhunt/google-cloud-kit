44f8d25b833f459fa10acd0408e653391dd0ec59
Owain Hunt <owain.hunt@johnlewis.co.uk>
2025-02-25 18:02:35 +0000

    Use protocol extension function for PubSubRequest generation

--- PubSub/Sources/PubSubClient.swift
+++ PubSub/Sources/PubSubClient.swift
@@ -15,28 +15,11 @@ public final class GoogleCloudPubSubClient {
         scope: [GoogleCloudPubSubScope]
     ) async throws {
         
-        let resolvedCredentials = try await CredentialsResolver.resolveCredentials(strategy: strategy)
-        
-        switch resolvedCredentials {
-            case .gcloud(let gCloudCredentials):
-                let provider = GCloudCredentialsProvider(client: client, credentials: gCloudCredentials)
-                pubSubRequest = .init(tokenProvider: provider, client: client, project: gCloudCredentials.quotaProjectId)
-                
-            case .serviceAccount(let serviceAccountCredentials):
-                let provider = ServiceAccountCredentialsProvider(client: client, credentials: serviceAccountCredentials, scope: scope)
-                pubSubRequest = .init(tokenProvider: provider, client: client, project: serviceAccountCredentials.projectId)
-                
-            case .computeEngine(let metadataUrl):
-                let projectId = ProcessInfo.processInfo.environment["PROJECT_ID"] ?? "default"
-                switch strategy {
-                    case .computeEngine(let client):
-                        let provider = ComputeEngineCredentialsProvider(client: client, scopes: scope, url: metadataUrl)
-                        pubSubRequest = .init(tokenProvider: provider, client: client, project: projectId)
-                    default:
-                        let provider = ComputeEngineCredentialsProvider(client: client, scopes: scope, url: metadataUrl)
-                        pubSubRequest = .init(tokenProvider: provider, client: client, project: projectId)
-                }
-        }
+        pubSubRequest = try await .request(
+            strategy: strategy,
+            client: client,
+            scope: scope
+        )
         
         pubSubTopic = GoogleCloudPubSubTopicsAPI(request: pubSubRequest, endpoint: base)
         pubSubSubscription = GoogleCloudPubSubSubscriptionsAPI(request: pubSubRequest, endpoint: base)
