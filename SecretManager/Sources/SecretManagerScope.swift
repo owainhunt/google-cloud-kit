728c8aebd15e500255a432b964d90bfad47f6eb1
Owain Hunt <owain.hunt@johnlewis.co.uk>
2025-02-25 14:16:34 +0000

    Add SecretManager scope

--- SecretManager/Sources/SecretManagerScope.swift
+++ SecretManager/Sources/SecretManagerScope.swift
@@ -9,7 +9,12 @@ import Core
 import Foundation
 
 public enum GoogleCloudSecretManagerScope: GoogleCloudAPIScope, CaseIterable {
+    /// See, edit, configure, and delete your Google Cloud data and see the email address for your Google Account.
+    case cloudPlatform
+    
     public var value: String {
-        ""
+        switch self {
+            case .cloudPlatform: return "https://www.googleapis.com/auth/cloud-platform"
+        }
     }
 }
